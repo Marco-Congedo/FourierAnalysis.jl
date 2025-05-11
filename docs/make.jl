e44f7b9982aa3818bd9c03bd90c2ab8345876b7b
Marco Congedo <marco.congedo@gmail.com>
2025-05-11 20:30:41 +0200

    dev

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,14 @@
+push!(LOAD_PATH,"../src/")
+push!(LOAD_PATH, @__DIR__)
 using Documenter
 using FourierAnalysis
 
 makedocs(
-    sitename = "FourierAnalysis",
-    format = Documenter.HTML(),
-    authors="Marco Congedo, CNRS, Grenoble, France; Livio Finos, Uni. Padova, Italia",
-    modules = [FourierAnalysis],
-    pages =  [
+   sitename="FourierAnalysis",
+   format = Documenter.HTML(),
+   authors="Marco Congedo, CNRS, France",
+   modules=[FourierAnalysis],
+   pages =  [
       "index.md",    
       "Main Module" => "MainModule.md",    
       "Tapering Windows" => "tapers.md",
@@ -14,31 +16,25 @@ makedocs(
                            "Spectral Estimations" => "spectra.md",
                            "Cross-Spectral Matrices" => "crossspectra.md",
                            "Coherence Matrices" => "coherence.md",
-                           "Goertzel's Algorithms" => "goertzel.md",
+                           "Goertzel's Algorithms" => "goertzel.md"
       ],
       "time-frequency(TF) domain" => Any[
                            "TF Representations" => "timefrequency.md",
                            "TF Univariate Measures" => "timefrequencyuni.md",
-                           "TF Bivariate Measures " => "timefrequencybi.md",
+                           "TF Bivariate Measures " => "timefrequencybi.md"
       ],
       "utilities" => Any[
                         "Plots" => "recipes.md",
                         "Tools" => "tools.md",
                         "FFTW planners"  => "fftw.md",
                         "Filter Banks" => "filters.md",
-                        "Hilbert Transform" => "hilbert.md",
+                        "Hilbert Transform" => "hilbert.md"
       ]
    ]
 )
 
 deploydocs(
-    # root,
-   # target = "build", # add this folder to .gitignore!
-   repo = "github.com/Marco-Congedo/FourierAnalysis.jl.git",
-   branch = "gh-pages",
-   # osname = "linux",
-   # deps = Deps.pip("pygments", "mkdocs"),
-   devbranch = "dev",
-   devurl = "dev",
-   # versions = ["stable" => "v^", "v#.#", devurl => devurl]
+    repo = "github.com/Marco-Congedo/FourierAnalysis.jl.git",
+    target = "build",
+    devurl = "dev"
 )
