f99d2d1a088cd993a4aacde97460f37198bfe97c
Marco Congedo <marco.congedo@gmail.com>
2025-05-11 18:47:38 +0200

    temp

--- docs/make.jl
+++ docs/make.jl
@@ -8,29 +8,7 @@ makedocs(
    format = Documenter.HTML(),
    authors="Marco Congedo, CNRS, France",
    modules=[FourierAnalysis],
-   pages =  [
-      "index.md",
-      "Main Module" => "MainModule.md",
-      "Tapering Windows" => "tapers.md",
-      "frequency domain" => Any[
-                           "Spectral Estimations" => "spectra.md",
-                           "Cross-Spectral Matrices" => "crossspectra.md",
-                           "Coherence Matrices" => "coherence.md",
-                           "Goertzel's Algorithms" => "goertzel.md",
-      ],
-      "time-frequency(TF) domain" => Any[
-                           "TF Representations" => "timefrequency.md",
-                           "TF Univariate Measures" => "timefrequencyuni.md",
-                           "TF Bivariate Measures " => "timefrequencybi.md",
-      ],
-      "utilities" => Any[
-                        "Plots" => "recipes.md",
-                        "Tools" => "tools.md",
-                        "FFTW planners"  => "fftw.md",
-                        "Filter Banks" => "filters.md",
-                        "Hilbert Transform" => "hilbert.md",
-      ]
-   ]
+   pages =  ["index.md"]
 )
 
 deploydocs(
